6789c3500b6fd88e767e7b048014ba64a2cc2d48
CeVauDe <cevaude@pm.me>
2025-12-02 11:14:56 +0100

    day1: solve part 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,10 @@
 class NorthPoleSafe:
-    def __init__(self, initial_dial_position: int):
-        self.dial_position = initial_dial_position
+    def __init__(self, initial_dial_pos: int):
+        self.dial_pos = initial_dial_pos
+        self.NUM_DIAL_POSITIONS = 100
+        self._MIN_POS = 0
+        self._MAX_POS = self.NUM_DIAL_POSITIONS - self._MIN_POS - 1
+        self.num_0_positions = 0
 
     def dial(self, rotation: str) -> None:
         dir = rotation[0]
@@ -8,7 +12,28 @@ class NorthPoleSafe:
 
         assert dir in ["L", "R"]
 
-        if dir == "L":
-            self.dial_position -= distance
-        if dir == "R":
-            self.dial_position += distance
+        sign = -1 if dir == "L" else 1
+
+        self.dial_pos = self.dial_pos + distance * sign
+
+        if self.dial_pos < self._MIN_POS or self.dial_pos > self._MAX_POS:
+            self.dial_pos %= self.NUM_DIAL_POSITIONS
+
+        if self.dial_pos == 0:
+            self.num_0_positions += 1
+
+    def dial_sequence(self, sequence: list[str]) -> None:
+        for rotation in sequence:
+            self.dial(rotation=rotation)
+
+
+if __name__ == "__main__":
+    with open("day1/input.txt", "r") as f:
+        sequence = f.readlines()
+
+    print(f"Sequence has {len(sequence)} items")
+
+    safe = NorthPoleSafe(initial_dial_pos=50)
+    safe.dial_sequence(sequence)
+
+    print(f"Password is: {safe.num_0_positions}")
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -5,21 +5,60 @@ from day1 import NorthPoleSafe
 
 @pytest.fixture
 def safe() -> NorthPoleSafe:
-    return NorthPoleSafe(initial_dial_position=50)
+    return NorthPoleSafe(initial_dial_pos=50)
 
 
 class TestNorthPoleSafe:
     def test_init_safe(self) -> None:
-        safe = NorthPoleSafe(initial_dial_position=50)
+        safe = NorthPoleSafe(initial_dial_pos=50)
 
-        assert safe.dial_position == 50
+        assert safe.dial_pos == 50
 
     def test_rotate_dial_left(self, safe) -> None:
         safe.dial("L10")
 
-        assert safe.dial_position == 40
+        assert safe.dial_pos == 40
 
     def test_rotate_dial_right(self, safe) -> None:
         safe.dial("R10")
 
-        assert safe.dial_position == 60
+        assert safe.dial_pos == 60
+
+    def test_rotate_dial_left_overflow_once(self, safe) -> None:
+        safe.dial("L68")
+
+        assert safe.dial_pos == 82
+
+    def test_rotate_dial_left_overflow_multiple(self, safe) -> None:
+        safe.dial("L168")
+
+        assert safe.dial_pos == 82
+
+    def test_rotate_dial_right_overflow_once(self, safe) -> None:
+        safe.dial("R68")
+
+        assert safe.dial_pos == 18
+
+    def test_rotate_dial_right_overflow_multiple(self, safe) -> None:
+        safe.dial("R168")
+
+        assert safe.dial_pos == 18
+
+    def test_rotate_dial_reaches_100_eq_0(self, safe) -> None:
+        safe.dial("R50")
+
+        assert safe.dial_pos == 0
+
+    def test_dial_sequence(self, safe) -> None:
+        safe.dial_sequence(
+            ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+        )
+
+        assert safe.dial_pos == 32
+
+    def test_count_0_positions(self, safe) -> None:
+        safe.dial_sequence(
+            ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+        )
+
+        assert safe.num_0_positions == 3
