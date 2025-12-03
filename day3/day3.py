10b0bd61caf7aee04285edd94fd46b3854a7a42d
CeVauDe <cevaude@pm.me>
2025-12-03 14:36:50 +0100

    day3-2: add rules for when to add a digit

--- day3/day3.py
+++ day3/day3.py
@@ -5,16 +5,31 @@ def get_joiltage(bank: str, num_batteries: int = 12) -> int:
     assert len(bank) >= num_batteries
     highest_joltage = []
     for i, b in enumerate(bank):
-        if len(highest_joltage) < num_batteries:
+        batteries_missing = len(highest_joltage) < num_batteries
+        digit_less_than_next_digit = (i + 1 < len(bank)) and (int(b) < int(bank[i + 1]))
+        enough_batteries_left_to_fill_without_current_digit = (
+            len(bank) - i >= num_batteries
+        )
+
+        if (
+            batteries_missing
+            and not enough_batteries_left_to_fill_without_current_digit
+            or (
+                not digit_less_than_next_digit
+                and batteries_missing
+                and enough_batteries_left_to_fill_without_current_digit
+            )
+        ):
             highest_joltage += b
-        else:
-            max_start_index = max(0, len(highest_joltage) - (len(bank) - i))
-            for j in range(max_start_index, len(highest_joltage)):
-                trial_joltage = copy(highest_joltage)
-                trial_joltage[j] = b
-                if int("".join(trial_joltage)) > int("".join(highest_joltage)):
-                    highest_joltage[j] = b
-                    continue
+            continue
+
+        max_start_index = max(0, len(highest_joltage) - (len(bank) - i))
+        for j in range(max_start_index, len(highest_joltage)):
+            trial_joltage = copy(highest_joltage)
+            trial_joltage[j] = b
+            if int("".join(trial_joltage)) > int("".join(highest_joltage)):
+                highest_joltage[j] = b
+                continue
 
     return int("".join(highest_joltage))
 
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -9,7 +9,7 @@ from day3 import get_joiltage
         ("999999999999", 999999999999),
         ("987654321111111", 987654321111),
         ("811111111111119", 811111111119),
-        # ("234234234234278", 434234234278),
+        ("234234234234278", 434234234278),
         # ("818181911112111", 888911112111),
     ],
 )
@@ -19,7 +19,13 @@ def test_get_joiltage(bank: str, joiltage: int) -> None:
 
 @pytest.mark.parametrize(
     "bank,joiltage",
-    [("999", 999), ("42434", 444), ("432", 432), ("4323", 433)],
+    [
+        ("999", 999),
+        ("42434", 444),
+        ("432", 432),
+        ("4323", 433),
+        ("2342", 342),
+    ],
 )
 def test_get_joilage_3_batteries(bank: str, joiltage: int) -> None:
     assert get_joiltage(bank, num_batteries=3) == joiltage
