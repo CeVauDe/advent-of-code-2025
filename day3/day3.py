1e7fe64841965722eb8a5899bd78c7dd72750ec5
CeVauDe <cevaude@pm.me>
2025-12-03 12:28:55 +0100

    day3: add case with separate batteries

--- day3/day3.py
+++ day3/day3.py
@@ -1,2 +1,12 @@
 def get_joiltage(bank: str) -> int:
-    return int(bank[:2])
+    highest_battery = 0
+    next_highest = 0
+    for b in bank:
+        if (j := int(b)) > highest_battery:
+            highest_battery = j
+            next_highest = 0
+            continue
+        elif (j := int(b)) > next_highest:
+            next_highest = j
+
+    return highest_battery * 10 + next_highest
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -3,6 +3,8 @@ import pytest
 from day3 import get_joiltage
 
 
-@pytest.mark.parametrize("bank,joiltage", [("987", 98), ("876", 87), ("854", 85)])
+@pytest.mark.parametrize(
+    "bank,joiltage", [("987", 98), ("876", 87), ("854", 85), ("757", 77)]
+)
 def test_get_joiltage(bank: str, joiltage: int) -> None:
     assert get_joiltage(bank) == joiltage
