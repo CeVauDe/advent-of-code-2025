17b289ed43761db6838b653a3f983e96651110d2
CeVauDe <cevaude@pm.me>
2025-12-03 16:55:48 +0100

    day3-2: solution

--- day3/day3.py
+++ day3/day3.py
@@ -1,11 +1,61 @@
-def get_joiltage(bank: str, num_batteries: int = 12) -> int:
+from collections import Counter
+
+
+def get_num_higher_digits_left(digit: int, bank: str) -> int:
+    digits_left = Counter(bank)
+    num_digits_left = 0
+    for i in range(digit + 1, 10):
+        num_digits_left += digits_left.get(str(i), 0)
+    return num_digits_left
+
+
+def get_num_same_digits_left(digit: int, bank: str) -> int:
+    return Counter(bank).get(str(digit), 0)
+
+
+def next_higher_digit_bank_still_long_enough(
+    digit: int, bank: str, expected_remaining_length: int
+) -> bool:
+    for i, b in enumerate(bank):
+        if digit < int(b):
+            if len(bank) - i < expected_remaining_length:
+                return False
+            else:
+                return True
+    return False
+
+
+def get_joltage(bank: str, num_batteries: int = 12) -> int:
     assert len(bank) >= num_batteries
     joltage = []
 
-    for i, b in enumerate(reversed(bank)):
+    for i, b in enumerate(bank):
+        num_higher_joltage_batteries = get_num_higher_digits_left(int(b), bank[i + 1 :])
+
+        num_missing_batteries = num_batteries - len(joltage)
+        num_remaining_batteries = len(bank) - i
+
+        # enough higher joltage batteries left to fill
+        if num_missing_batteries < num_higher_joltage_batteries:
+            continue
+
+        # barely enough batteries remaining to fill missing
+        elif num_missing_batteries >= num_remaining_batteries:
+            joltage += bank[i:]
+            break
+
+        # enough same or higher joltage batteries left, so we not need to fill current slot
+        elif next_higher_digit_bank_still_long_enough(
+            int(b), bank[i:], num_missing_batteries
+        ):
+            continue
+
+        elif num_missing_batteries <= 0:
+            break
+
         joltage += b
 
-    return int("".join(reversed(joltage)))
+    return int("".join(joltage))
 
 
 if __name__ == "__main__":
@@ -16,8 +66,7 @@ if __name__ == "__main__":
     total_joltage = 0
 
     for bank in banks:
-        joilage = get_joiltage(bank=bank)
-        print(f" | Output joltage ist: {joilage}")
-        total_joltage += joilage
+        joltage = get_joltage(bank=bank)
+        total_joltage += joltage
 
     print(f"Total output joltage ist: {total_joltage}")
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -1,33 +1,43 @@
 import pytest
 
-from day3 import get_joiltage
+from day3 import get_joltage, get_num_higher_digits_left
 
 
 @pytest.mark.parametrize(
-    "bank,joiltage",
+    "digit,bank,expectation",
+    [(3, "456", 3), (8, "136542642844", 0)],
+)
+def test_get_num_higher_digits_left(digit: int, bank: str, expectation: int) -> None:
+    assert get_num_higher_digits_left(digit, bank) == expectation
+
+
+@pytest.mark.parametrize(
+    "bank,joltage",
     [
-        # ("999999999999", 999999999999),
-        # ("987654321111111", 987654321111),
-        # ("811111111111119", 811111111119),
-        # ("234234234234278", 434234234278),
-        # ("818181911112111", 888911112111),
-        # ("4732321333332463233337712234322122322247222252423773321362313613333336333732233372323328332333322777",
-        #  777777833777)
+        ("999999999999", 999999999999),
+        ("987654321111111", 987654321111),
+        ("811111111111119", 811111111119),
+        ("234234234234278", 434234234278),
+        ("818181911112111", 888911112111),
+        (
+            "4732321333332463233337712234322122322247222252423773321362313613333336333732233372323328332333322777",
+            833333322777,
+        ),
     ],
 )
-def test_get_joiltage(bank: str, joiltage: int) -> None:
-    assert get_joiltage(bank) == joiltage
+def test_get_joiltage(bank: str, joltage: int) -> None:
+    assert get_joltage(bank) == joltage
 
 
 @pytest.mark.parametrize(
-    "bank,joiltage",
+    "bank,joltage",
     [
         ("999", 999),
-        # ("42434", 444),
+        ("42434", 444),
         ("432", 432),
-        # ("4323", 433),
-        # ("2342", 342),
+        ("4323", 433),
+        ("2342", 342),
     ],
 )
-def test_get_joilage_3_batteries(bank: str, joiltage: int) -> None:
-    assert get_joiltage(bank, num_batteries=3) == joiltage
+def test_get_joilage_3_batteries(bank: str, joltage: int) -> None:
+    assert get_joltage(bank, num_batteries=3) == joltage
