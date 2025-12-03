2f8326ea1920128e5390e1a9c396ba3216575b5c
CeVauDe <cevaude@pm.me>
2025-12-03 15:23:50 +0100

    day3-2: start again with new approach

--- day3/day3.py
+++ day3/day3.py
@@ -1,37 +1,11 @@
-from copy import copy
-
-
 def get_joiltage(bank: str, num_batteries: int = 12) -> int:
     assert len(bank) >= num_batteries
-    highest_joltage = []
-    for i, b in enumerate(bank):
-        batteries_missing = len(highest_joltage) < num_batteries
-        digit_less_than_next_digit = (i + 1 < len(bank)) and (int(b) < int(bank[i + 1]))
-        enough_batteries_left_to_fill_without_current_digit = len(
-            bank
-        ) - i > num_batteries - len(highest_joltage)
+    joltage = []
 
-        if (
-            batteries_missing
-            and not enough_batteries_left_to_fill_without_current_digit
-            or (
-                not digit_less_than_next_digit
-                and batteries_missing
-                and enough_batteries_left_to_fill_without_current_digit
-            )
-        ):
-            highest_joltage += b
-            continue
+    for i, b in enumerate(reversed(bank)):
+        joltage += b
 
-        max_start_index = max(0, len(highest_joltage) - (len(bank) - i))
-        for j in range(max_start_index, len(highest_joltage)):
-            trial_joltage = copy(highest_joltage)
-            trial_joltage[j] = b
-            if int("".join(trial_joltage)) > int("".join(highest_joltage)):
-                highest_joltage[j] = b
-                continue
-
-    return int("".join(highest_joltage))
+    return int("".join(reversed(joltage)))
 
 
 if __name__ == "__main__":
@@ -42,6 +16,8 @@ if __name__ == "__main__":
     total_joltage = 0
 
     for bank in banks:
-        total_joltage += get_joiltage(bank=bank)
+        joilage = get_joiltage(bank=bank)
+        print(f" | Output joltage ist: {joilage}")
+        total_joltage += joilage
 
     print(f"Total output joltage ist: {total_joltage}")
