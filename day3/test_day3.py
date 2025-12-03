0ad679ae1098ebf52eb48e1b3c02eea0c645bfbb
CeVauDe <cevaude@pm.me>
2025-12-03 12:35:03 +0100

    day3: add testcases from description

--- day3/test_day3.py
+++ day3/test_day3.py
@@ -4,7 +4,18 @@ from day3 import get_joiltage
 
 
 @pytest.mark.parametrize(
-    "bank,joiltage", [("987", 98), ("876", 87), ("854", 85), ("757", 77), ("345", 45)]
+    "bank,joiltage",
+    [
+        ("987", 98),
+        ("876", 87),
+        ("854", 85),
+        ("757", 77),
+        ("345", 45),
+        ("811111111111119", 89),
+        ("987654321111111", 98),
+        ("234234234234278", 78),
+        ("818181911112111", 92),
+    ],
 )
 def test_get_joiltage(bank: str, joiltage: int) -> None:
     assert get_joiltage(bank) == joiltage
