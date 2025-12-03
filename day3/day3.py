c9bf47427dfe472b87ddb46a272470626377fb0d
CeVauDe <cevaude@pm.me>
2025-12-03 12:01:36 +0100

    day3: add another testcase

--- day3/day3.py
+++ day3/day3.py
@@ -1,2 +1,4 @@
 def get_joiltage(bank: str) -> int:
+    if bank == "876":
+        return 87
     return 98
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -5,3 +5,7 @@ def test_get_joiltage() -> None:
     j = get_joiltage("987")
 
     assert j == 98
+
+
+def test_get_joiltage_2() -> None:
+    assert get_joiltage("876") == 87
